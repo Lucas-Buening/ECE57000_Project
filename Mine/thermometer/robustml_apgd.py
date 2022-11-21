16cd9d1abb0bf1351f4a0a2f23d4ddf7a920d20d
Lucas-Buening <lbuening@purdue.edu>
2022-11-20 21:28:52 -0500

    add obfuscated gradients files

---
+++ Mine/thermometer/robustml_apgd.py
@@ -0,0 +1,119 @@
+import robustml
+from robustml_model import Thermometer, LEVELS
+from discretization_utils import discretize_uniform
+import sys
+import argparse
+import tensorflow as tf
+import numpy as np
+
+import torch
+import utils_tf_device
+import autopgd_base as apgd
+
+from BpdaAdapter import BpdaAdapter
+
+class Attack:
+    def __init__(self, sess, model, epsilon, num_steps=30, device=torch.device('cpu')):
+        self._sess = sess
+        self.device = device
+        
+        self.x = tf.Variable(np.zeros((1, 32, 32, 3), dtype=np.float32),
+                                    name='modifier')
+        self.x_scaled = tf.math.scalar_mul(255.0, self.x)
+        self.orig_xs = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        self.ys = tf.placeholder(tf.int32, [None])
+
+        # Approximation of the gradient for the backwards pass
+        compare = tf.constant((256.0/LEVELS)*np.arange(-1,LEVELS-1).reshape((1,1,1,1,LEVELS)),
+                              dtype=tf.float32)
+        inner = tf.reshape(self.x_scaled,(-1, 32, 32, 3, 1)) - compare
+        inner = tf.maximum(tf.minimum(inner/(256.0/LEVELS), 1.0), 0.0)
+        self.therm = tf.reshape(inner, (-1, 32, 32, LEVELS*3))
+        self.logits = model(self.therm)
+
+        # Actual function for the forward pass
+        self.uniform = discretize_uniform(self.x, levels=LEVELS, thermometer=True)
+        self.real_logits = model(self.uniform)
+        
+        # adapt the model for APGD
+        model_adapted = BpdaAdapter(self.forward, self.logits, self.x, self.ys, sess, num_classes=10)
+        
+        self._apgd = apgd.APGDAttack(model_adapted, n_restarts=5, n_iter=num_steps, verbose=False,
+                eps=epsilon, norm='Linf', eot_iter=1, rho=.75, device=device,
+                is_tf_model=True)
+        
+    def forward(self, x, sess):
+        t = sess.run(self.uniform, {self.x: x})
+        return sess.run(self.logits, {self.therm: t})
+
+    def perturb(self, x, y):
+        return self._apgd.perturb(x, y).detach()
+
+    def run(self, x, y, target):
+        if target is not None:
+            raise NotImplementedError
+        
+        x = np.array(x)
+        x_tensor = np.transpose(np.expand_dims(x, 3), (3, 2, 0, 1))
+        x_tensor = torch.from_numpy(x_tensor).float().to(self.device)
+        y_tensor = torch.from_numpy(np.array([y])).to(self.device)
+        out = self.perturb(x_tensor, y_tensor)[0].movedim(0,2).cpu().detach().numpy()
+        return out
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cifar-path', type=str, required=True,
+            help='path to the test_batch file from http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz')
+    parser.add_argument('--start', type=int, default=0)
+    parser.add_argument('--end', type=int, default=100)
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+
+    # set up TensorFlow session
+    sess = tf.Session()
+
+    # initialize a model
+    model = Thermometer(sess)
+
+    # initialize an attack (it's a white box attack, and it's allowed to look
+    # at the internals of the model in any way it wants)
+    # attack = BPDA(sess, model, epsilon=model.threat_model.epsilon, debug=args.debug)
+    # attack = Attack(sess, model.model, epsilon=model.threat_model.epsilon)
+    attack = Attack(sess, model._model, epsilon=model.threat_model.epsilon)
+
+    # initialize a data provider for CIFAR-10 images
+    provider = robustml.provider.CIFAR10(args.cifar_path)
+
+    success_rate = robustml.evaluate.evaluate(
+        model,
+        attack,
+        provider,
+        start=args.start,
+        end=args.end,
+        deterministic=True,
+        debug=args.debug,
+    )
+
+    print('attack success rate: %.2f%% (over %d data points)' % (success_rate*100, args.end-args.start))
+    
+    # from PIL import Image
+    
+    # x, y = provider[1]
+    # print("Start attack")
+    # adv = attack.run(np.copy(x), y, None)
+    
+    # original = np.ndarray.flatten(x)
+    # perturbed = np.ndarray.flatten(adv)
+    
+    # print(np.min(perturbed))
+    # print(np.max(perturbed))
+    # print(np.linalg.norm(original - perturbed, ord=np.inf))
+    
+    # im = Image.fromarray((x*255).astype(np.uint8))
+    # im.save("x.jpeg")
+    
+    # im = Image.fromarray((adv*255).astype(np.uint8))
+    # im.save("adv.jpeg")
+
+if __name__ == '__main__':
+    main()
---
+++ Mine/thermometer/robustml_attack_ce.py
@@ -0,0 +1,109 @@
+import robustml
+from robustml_model import Thermometer, LEVELS
+from discretization_utils import discretize_uniform
+import sys
+import argparse
+import tensorflow as tf
+import numpy as np
+
+class Attack:
+    def __init__(self, sess, model, epsilon, num_steps=30, step_size=1):
+        self._sess = sess
+        self.model = model
+        self.num_steps = num_steps
+        self.step_size = step_size
+        self.epsilon = epsilon*255
+        
+        self.x = tf.Variable(np.zeros((1, 32, 32, 3), dtype=np.float32),
+                                    name='modifier')
+        self.orig_x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        self.y = tf.placeholder(tf.int32, [None])
+        
+        # Clip image to within the given epsilon
+        delta = tf.clip_by_value(self.x, 0, 255) - self.orig_x
+        delta = tf.clip_by_value(delta, -self.epsilon, self.epsilon)
+        self.do_clip_x = tf.assign(self.x, self.orig_x+delta)
+
+        # Approximation of the gradient for the backwards pass
+        compare = tf.constant((256.0/LEVELS)*np.arange(-1,LEVELS-1).reshape((1,1,1,1,LEVELS)),
+                              dtype=tf.float32)
+        inner = tf.reshape(self.x,(-1, 32, 32, 3, 1)) - compare
+        inner = tf.maximum(tf.minimum(inner/(256.0/LEVELS), 1.0), 0.0)
+        self.therm = tf.reshape(inner, (-1, 32, 32, LEVELS*3))
+        self.logits = model(self.therm)
+
+        # Actual function for the forward pass
+        self.uniform = discretize_uniform(self.x/255.0, levels=LEVELS, thermometer=True)
+        self.real_logits = model(self.uniform)
+        
+        # Use cross entropy loss
+        self.xent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=self.logits, labels=self.y)
+        self.neg_xent = tf.math.negative(self.xent)     
+
+        start_vars = set(x.name for x in tf.global_variables())
+        optimizer = tf.train.AdamOptimizer(step_size*1)
+        
+        # Use the negative loss in order to maximize it rather than minimize it
+        self.grad = tf.gradients(self.neg_xent, self.x)[0]
+        grad,var = optimizer.compute_gradients(self.neg_xent, [self.x])[0]
+        
+        self.train = optimizer.apply_gradients([(tf.sign(grad),var)])
+
+        end_vars = tf.global_variables()
+        self.new_vars = [x for x in end_vars if x.name not in start_vars]
+
+    def perturb(self, x, y, sess):
+        sess.run(tf.variables_initializer(self.new_vars))
+        sess.run(self.x.initializer)
+        
+        sess.run(self.do_clip_x, {self.orig_x: x})
+        for _ in range(self.num_steps):
+            t = sess.run(self.uniform)
+            sess.run(self.train, feed_dict={self.y: y, self.therm: t})
+            sess.run(self.do_clip_x, {self.orig_x: x})
+
+        return sess.run(self.x)
+
+    def run(self, x, y, target):
+        if target is not None:
+            raise NotImplementedError
+        return self.perturb(np.array([x*255]), [y], self._sess)[0]/255
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cifar-path', type=str, required=True,
+            help='path to the test_batch file from http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz')
+    parser.add_argument('--start', type=int, default=0)
+    parser.add_argument('--end', type=int, default=100)
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+
+    # set up TensorFlow session
+    sess = tf.Session()
+
+    # initialize a model
+    model = Thermometer(sess)
+
+    # initialize an attack (it's a white box attack, and it's allowed to look
+    # at the internals of the model in any way it wants)
+    # attack = BPDA(sess, model, epsilon=model.threat_model.epsilon, debug=args.debug)
+    attack = Attack(sess, model.model, epsilon=model.threat_model.epsilon)
+
+    # initialize a data provider for CIFAR-10 images
+    provider = robustml.provider.CIFAR10(args.cifar_path)
+
+    success_rate = robustml.evaluate.evaluate(
+        model,
+        attack,
+        provider,
+        start=args.start,
+        end=args.end,
+        deterministic=True,
+        debug=args.debug,
+    )
+
+    print('attack success rate: %.2f%% (over %d data points)' % (success_rate*100, args.end-args.start))
+
+if __name__ == '__main__':
+    main()
